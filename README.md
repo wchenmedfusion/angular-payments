7b2cc958f3a6c8a82cf453fee8236644e0a1c255
Omar Meky <omeky@medfusion.com>
2015-07-06 12:01:24 -0400

    Default to no card types supported; added ability to set allowed card types, removed stripe form

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A fork of angular-payments that limits support to amex, disocver, visa, and mastercard
+A fork of angular-payments that limits support to amex, discover, visa, and mastercard
+
+It defaults to no cards supported but allows the user to set the supported types.
--- src/cards.js
+++ src/cards.js
@@ -1,82 +1,86 @@
 angular.module('angularPayments')
 
-.factory('Cards', [function(){
+.factory('Cards', [function() {
 
-  var defaultFormat = /(\d{1,4})/g;
-  var defaultInputFormat =  /(?:^|\s)(\d{4})$/;
+    var defaultFormat = /(\d{1,4})/g;
+    var defaultInputFormat = /(?:^|\s)(\d{4})$/;
 
-        var cards = [
-    {
-      type: 'discover',
-      pattern: /^(6011|65|64[4-9]|622)/,
-      format: defaultFormat,
-      inputFormat: defaultInputFormat,
-      length: [16],
-      cvcLength: [3],
-      luhn: true
-    }, {
-      type: 'mastercard',
-      pattern: /^5[1-5]/,
-      format: defaultFormat,
-      inputFormat: defaultInputFormat,
-      length: [16],
-      cvcLength: [3],
-      luhn: true
-    }, {
-      type: 'amex',
-      pattern: /^3[47]/,
-      format: /(\d{1,4})(\d{1,6})?(\d{1,5})?/,
-      inputFormat: /^(\d{4}|\d{4}\s\d{6})$/,
-      length: [15],
-      cvcLength: [3, 4],
-      luhn: true
-    }, {
-      type: 'visa',
-      pattern: /^4/,
-      format: defaultFormat,
-      inputFormat: defaultInputFormat,
-      length: [13, 14, 15, 16],
-      cvcLength: [3],
-      luhn: true
-    }
-  ];
-
-
-  var _fromNumber = function(num){
-      var card, i, len;
-
-      num = (num + '').replace(/\D/g, '');
-
-      for (i = 0, len = cards.length; i < len; i++) {
-
-        card = cards[i];
-
-        if (card.pattern.test(num)) {
-          return card;
-        }
-
+    var cards = {
+      discover: {
+        type: 'discover',
+        pattern: /^(6011|65|64[4-9]|622)/,
+        format: defaultFormat,
+        inputFormat: defaultInputFormat,
+        length: [16],
+        cvcLength: [3],
+        luhn: true
+      },
+      mastercard: {
+        type: 'mastercard',
+        pattern: /^5[1-5]/,
+        format: defaultFormat,
+        inputFormat: defaultInputFormat,
+        length: [16],
+        cvcLength: [3],
+        luhn: true
+      },
+      amex: {
+        type: 'amex',
+        pattern: /^3[47]/,
+        format: /(\d{1,4})(\d{1,6})?(\d{1,5})?/,
+        inputFormat: /^(\d{4}|\d{4}\s\d{6})$/,
+        length: [15],
+        cvcLength: [3, 4],
+        luhn: true
+      },
+      visa: {
+        type: 'visa',
+        pattern: /^4/,
+        format: defaultFormat,
+        inputFormat: defaultInputFormat,
+        length: [13, 14, 15, 16],
+        cvcLength: [3],
+        luhn: true
       }
-  }
-
-  var _fromType = function(type) {
-      var card, i, len;
-
-      for (i = 0, len = cards.length; i < len; i++) {
-
-        card = cards[i];
-        
-        if (card.type === type) {
-          return card;
-        }
-
-      }
-  };
+    };
 
   return {
-      fromNumber: function(val) { return _fromNumber(val) },
-      fromType: function(val) { return _fromType(val) },
-      defaultFormat: function() { return defaultFormat;},
-      defaultInputFormat: function() { return defaultInputFormat;}
+      cards: [],
+      setCards: function(cardTypes) {
+        this.cards = [];
+        for (var i = 0; i < cardTypes.length; i++) {
+            this.cards.push(cards[cardTypes[i]]);
+        }
+      },
+      fromNumber: function(num){
+          var card, i, len;
+
+          num = (num + '').replace(/\D/g, '');
+
+          for (i = 0, len = this.cards.length; i < len; i++) {
+
+            card = this.cards[i];
+
+            if (card.pattern.test(num)) {
+              return card;
+            }
+
+          }
+      },
+      fromType: function(type) {
+          var card, i, len;
+
+          for (i = 0, len = this.cards.length; i < len; i++) {
+
+            card = this.cards[i];
+
+            if (card.type === type) {
+              return card;
+            }
+
+          }
+      },
+      defaultInputFormat: /(?:^|\s)(\d{4})$/
   }
 
 }])
